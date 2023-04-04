d07316c83b144501c1b0809835d61c9b590d9ef7
Rahul Saini <111075975+Rahulnerdy@users.noreply.github.com>
2023-04-04 13:25:11 +0530

    Update ATMInterface.java

--- ClassAssignments/ATMInterface.java
+++ ClassAssignments/ATMInterface.java
@@ -7,7 +7,7 @@ public class ATMInterface {
         Scanner in = new Scanner(System.in);
         int balance,pin,amount,EnteredPin,i=2,n;
         pin = 1234;
-        balance = 5000;
+        balance = 50000;
         System.out.print("Enter Your Pin Number:  ");
         EnteredPin = in.nextInt();
         while(EnteredPin!=pin && i>0){
@@ -19,21 +19,21 @@ public class ATMInterface {
         if(i==0){
             System.out.println("You have entered Wrong Pin 3 times");
             System.out.println("ATM Blocked ! Contact Your Nearest Branch");
-            System.out.println("Thank you for using SBI bank! Have a good day");
-            System.exit(0);
+            System.out.println(Messages.GoodBye);
+            System.exit(1);
         }
-        System.out.println("    *******Welcome to SBI Bank******");
+        System.out.println("    ******* Welcome to RS Bank ******");
         System.out.print("Press 1 for Balance Check or Press 2 For withdraw: ");
         n = in.nextInt();
         if(n==1){
             System.out.println("Your Balance is "+ balance);
-            System.out.println("Thank you for using SBI bank! Have a good day");
+            System.out.println(Messages.GoodBye);
         } else if (n==2) {
             System.out.print("Enter the amount you want to withdraw: ");
             amount = in.nextInt();
             if(amount>balance){
                 System.out.println("Insufficient Amount Entered");
-                System.exit(0);
+                System.exit(1);
             }
             System.out.println("!!!! Collect Your Money !!!!");
             try{
@@ -43,8 +43,11 @@ public class ATMInterface {
             }
             System.out.println("Money Withdraw Successful");
             System.out.println("Remaining Balance = " + (balance - amount));
-            System.out.println("Thank you for using SBI bank! Have a good day");
+            System.out.println(Messages.GoodBye);
         }
 
     }
+}
+class Messages{
+    static String GoodBye = "Thank you for using RS bank! Have a good day";
 }
