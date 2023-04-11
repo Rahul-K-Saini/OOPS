17f06d8f52d165959f5ea88fa441facbfe9814a7
Rahul Saini <111075975+Rahul-K-Saini@users.noreply.github.com>
2023-04-11 13:04:35 +0530

    Update Main.java

--- ClassAssignments/Multilevel inheritance/Main.java
+++ ClassAssignments/Multilevel inheritance/Main.java
@@ -1,19 +1,39 @@
-package ClassAssignments; // Package (Folder Name)
-
-import java.util.Scanner; // Importing utility Class Scanner to take input from user
-
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in); // Creating the object of Scanner Class
-        Ternary even = new Ternary(); // And an object of Ternary Class // Here the object name is even to check if no. is even
-        System.out.print("Enter a number to check if its Even or Odd: ");
-        even.num = in.nextInt(); //Assigning the user  input value to the Instance variable
-        String res = (even.num%2 == 0 || even.num ==0 ) ? "Even" : "Odd"; // use of Ternary operator to print Even or Odd
-        System.out.println(res);
-        Ternary vote = new Ternary(); // New object of Ternary Class called vote for eligibility of candidate to vote
-        System.out.print("Please Enter your age: ");
-        vote.age = in.nextInt(); // Same Taking input of their age from user
-        System.out.println((vote.age>=18) ? "You can vote": "You can't vote");
+        Child obj = new Child(100,2,2);
+        obj.displaySI();
+    }
 
+}
+class Child extends Parent {
+   Child(int p, int r, int t) {
+          super(p,r,t);
+    }
+    void displaySI(){
+        System.out.println("The SI is" + this.SI(this.p, this.r, this.t));
+    }
+
+}
+class Base{
+    int p;
+    int r;
+    int t;
+   
+    Base(int p,int r,int t){
+        this.p = p;
+        this.r = r;
+        this.t =t;
     }
 }
+class Parent extends Base{
+
+    Parent(int p, int r, int t) {
+        super(p, r, t);
+    }
+    double SI(int p,int r,int t){
+        double si = (p*r*t)/100;
+        return si;
+    }
+    
+}
+
